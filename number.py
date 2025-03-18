8e4556b75c7de4fca845ac03c740c39cde9067b9
Rahul Anandh <rahulanandh80@gmail.com>
2025-03-18 12:41:03 +0530

    number

---
+++ number.py
@@ -0,0 +1,11 @@
+print(10 + 3) #Addidtion
+print(10 - 3) #Substraction
+print(10 * 3) # Multiplication
+print(10 / 3) # Division with Floating points
+print(10 // 3)  # Division without Floating points called intiger
+print(10 ** 3) # Power
+print(10 % 3) #remainder
+
+x = 10
+x += 1
+print(x)  #Augmented Assignment Operators
---
+++ string.py
@@ -0,0 +1,6 @@
+my_name = "my name is rahul anandh"
+print(my_name.upper().strip())
+print(my_name.find("rahul"))
+print(my_name.replace("rahul","Gokul"))
+print("rahul" in my_name)
+print("gokul" not in my_name)
